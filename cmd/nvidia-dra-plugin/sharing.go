9de24f5bf4f8edecea9133de632c350c1893c232
wawa0210 <xiao.zhang@daocloud.io>
2024-01-24 23:50:46 +0800

    skip old architecture version GPU settings time slice
    Signed-off-by: wawa0210 <xiao.zhang@daocloud.io>

--- cmd/nvidia-dra-plugin/sharing.go
+++ cmd/nvidia-dra-plugin/sharing.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"text/template"
 	"time"
@@ -101,6 +102,19 @@ func (t *TimeSlicingManager) SetTimeSlice(devices *PreparedDevices, config *nasc
 		return fmt.Errorf("setting a TimeSlice duration on MIG devices is unsupported")
 	}
 
+	var supportTimeSliceIDs []string
+	for _, gpu := range devices.Gpu.Devices {
+		err, isSupportTimeSlice := detectSupportTimeSliceByCudaComputeCapability(gpu.cudaComputeCapability)
+		if err != nil {
+			return fmt.Errorf("failed to detectSupportTimeSliceByCudaComputeCapability : %w", err)
+		}
+		if isSupportTimeSlice {
+			supportTimeSliceIDs = append(supportTimeSliceIDs, gpu.uuid)
+			continue
+		}
+		klog.InfoS("the current card does not support setting time slices and will be ignored.", "arch", gpu.architecture, "uuid", gpu.uuid, "cudaComputeCapability", gpu.cudaComputeCapability)
+	}
+
 	timeSlice := nascrd.DefaultTimeSlice
 	if config != nil && config.TimeSlice != nil {
 		timeSlice = *config.TimeSlice
@@ -111,7 +125,7 @@ func (t *TimeSlicingManager) SetTimeSlice(devices *PreparedDevices, config *nasc
 		return fmt.Errorf("error setting compute mode: %w", err)
 	}
 
-	err = t.nvdevlib.setTimeSlice(devices.UUIDs(), timeSlice.Int())
+	err = t.nvdevlib.setTimeSlice(supportTimeSliceIDs, timeSlice.Int())
 	if err != nil {
 		return fmt.Errorf("error setting time slice: %w", err)
 	}
@@ -388,4 +402,22 @@ func (m *MpsControlDaemon) Stop(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+// detactSupportTimeSliceByArch Determine whether the architecture series
+// supports setting time slices based on the gpu cudaComputeCapability.
+func detectSupportTimeSliceByCudaComputeCapability(cudaComputeCapability string) (error, bool) {
+	// ref https://github.com/NVIDIA/k8s-dra-driver/pull/58#discussion_r1469338562
+	// we believe time-slicing is available on Volta+ architectures, so the check would simply be cudaComputeCapability >= 7.0
+	// by https://github.com/NVIDIA/go-nvlib/blob/main/pkg/nvlib/device/device.go#L149, We know that cuda major and minor versions are concatenated through `.` .
+
+	cudaVersion := strings.Split(cudaComputeCapability, ".")
+	major, err := strconv.Atoi(cudaVersion[0])
+	if err != nil {
+		return fmt.Errorf("error to get cudaComputeCapability major version %v", cudaComputeCapability), false
+	}
+	if major >= 7 {
+		return nil, true
+	}
+	return nil, false
 }
